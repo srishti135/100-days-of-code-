a9d3cb096270bee4d4aa4150c0c712c677f0fc64
Srishti jain <srishtijain906@gmail.com>
2025-09-04 20:05:00 +0530

    day5 q1 added

---
+++ day5.c
@@ -0,0 +1,13 @@
+//problem:Q9: Write a program to calculate simple and compound interest for given principal, rate, and time.
+#include <stdio.h>
+#include <math.h>
+
+int main(void) {
+    float principal, rate, time, simple_interest, compound_interest;
+    printf("Enter principal, rate and time: ");
+    scanf("%f %f %f", &principal, &rate, &time);
+    simple_interest = (principal * rate * time) / 100;
+    compound_interest = principal * pow((1 + rate / 100), time) - principal;
+    printf("Simple Interest=%.2f, Compound Interest=%.2f\n", simple_interest, compound_interest);
+    return 0;
+}
