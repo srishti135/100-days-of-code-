f13887915da20a041f8e8d1140155a22b3fac47c
Srishti jain <srishtijain906@gmail.com>
2025-09-19 14:43:47 +0530

    add day21q2

---
+++ day21q2.c
@@ -0,0 +1,23 @@
+//problem:Q42: Write a program to check if a number is a perfect number.
+#include <stdio.h>
+
+int main() {
+    int num, sum = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    for (int i = 1; i <= num / 2; i++) {
+        if (num % i == 0) {
+            sum += i;
+        }
+    }
+
+    if (sum == num) {
+        printf("Perfect number\n");
+    } else {
+        printf("Not perfect number\n");
+    }
+
+    return 0;
+}
