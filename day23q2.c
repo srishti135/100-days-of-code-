4c12ae9b53ef1d5651680f4976476e75f99820fd
Srishti jain <srishtijain906@gmail.com>
2025-09-21 13:50:50 +0530

    add day23q2

---
+++ day23q2.c
@@ -0,0 +1,22 @@
+/*problem:Q46: Write a program to print the following pattern:
+*****
+*****
+*****
+*****
+******
+*/
+#include <stdio.h>
+int main() {
+    int rows, cols;
+    printf("Enter the number of rows: ");
+    scanf("%d", &rows);
+    printf("Enter the number of columns: ");
+    scanf("%d", &cols);
+    for (int i = 1; i <= rows; i++) {
+        for (int j = 1; j <= cols; j++) {   
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
