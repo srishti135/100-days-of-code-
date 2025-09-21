f406818521efca0679d1869014daedd9daca3242
Srishti jain <srishtijain906@gmail.com>
2025-09-21 13:55:28 +0530

    add day24q1

---
+++ day24.c
@@ -0,0 +1,21 @@
+/*problem: Q47: Write a program to print the following pattern:
+*
+**
+***
+****
+*****
+
+*/
+#include <stdio.h>
+int main() {
+    int rows;
+    printf("Enter the number of rows: ");
+    scanf("%d", &rows);
+    for (int i = 1; i <= rows; i++) {       
+        for (int j = 1; j <= i; j++) {   
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}   
