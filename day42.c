2c4b4e9659ddda62903c687b6fa7a2677370e3fe
Srishti jain <srishtijain906@gmail.com>
2025-10-05 10:41:56 +0530

    add day42q1

---
+++ day42.c
@@ -0,0 +1,23 @@
+//problem:Q85: Reverse a string.
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[100], temp;
+    int i, j;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+    str[strcspn(str, "\n")] = 0;  // Remove newline character
+
+    // Reverse the string
+    j = strlen(str) - 1;
+    for (i = 0; i < j; i++, j--) {
+        temp = str[i];
+        str[i] = str[j];
+        str[j] = temp;
+    }
+
+    printf("Reversed string: %s\n", str);
+    return 0;
+}
